59abb5ba6fea693a30dd0cb35fe29d151c95606a
eeeonn <119471104+mieum12@users.noreply.github.com>
2023-02-24 23:38:25 +0900

    Update 삼각형의 완성조건 （2）.js

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -17,3 +17,14 @@ function solution(sides) {
     
     return answer;
 }
+
+//다른풀이 1
+// function solution(sides) {
+//   let answer = 0;
+//   let arr = sides.sort((a, b) => b - a);
+//   for (let i = arr[0]; arr[1] + i > arr[0]; i--) {
+//     answer++;
+//   }
+//   answer += arr[1] - 1;
+//   return answer;
+// }
