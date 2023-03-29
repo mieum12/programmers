ed686b075137a18c9d864dc50d630cbba5b7fdf6
mieum12 <qpdlqltb1215@gmail.com>
2023-03-29 12:08:55 +0900

    Update 팩토리얼.js

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -1,8 +1,23 @@
 function solution(n) {
-    let i = 1;
-    let ans = 1;
-    while (ans*i < n) {
-        ans *= ++i;
+  let ans = 1;
+  for(let i = 1; i <= n; i++){
+      ans *= i;
+        if(ans === n){
+        return i;
     }
-    return i;
+      
+      if(ans > n){
+          return i - 1;
+      }
+  }
+}
+
+//다른 풀이
+function solution(n) {
+  let i = 1;
+  let ans = 1;
+  while (ans*i < n) {
+      ans *=++i; //이 풀이의 뜻을 잘 모르겠음
+  }
+  return i;
 }
